25815e9b54081df3945206cacb3caf9f1a7c8f76
Alauddin Husain <alauddinkuet@gmail.com>
2017-07-08 01:39:06 +0600

    update

--- views/template_front.php
+++ views/template_front.php
@@ -95,7 +95,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; Your Website <?php echo date('Y');?></p>
                 </div>
             </div>
         </footer>
